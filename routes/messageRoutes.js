29f6fb817b011327adc698ae9c90624339b5b3dd
alveslimati <alveslima.ti@hotmail.com>
2025-05-31 19:51:05 -0300

    add messageRouters correct

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,4 +1,3 @@
-// routes/messageRoutes.js
 import express from 'express';
 import messageController from '../controllers/MessageController.js';
 import checkToken from '../middleware/auth.js'; // Importa o middleware
@@ -7,8 +6,7 @@ const router = express.Router();
 
 // Aplica o middleware checkToken a esta rota
 router.get('/', checkToken, messageController.getMessages);
-router.get('/api/messages', checkToken, MessageController.getMessages);
-router.get('/api/messages/messagesByDate', checkToken, MessageController.getMessagesByDate);
-
+router.get('/api/messages', checkToken, messageController.getMessages);
+router.get('/api/messages/messagesByDate', checkToken, messageController.getMessagesByDate);
 
 export default router;
