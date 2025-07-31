80b9b57271a4180eb8dec74d85405806e246b392
Jonatan Alves <alveslima.ti@hotmail.com>
2025-07-31 18:56:04 -0300

    ajusting import middleware

--- routes/messageRealTimeRoutes.js
+++ routes/messageRealTimeRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import MessageRealTimeController from '../controllers/MessageRealTimeController.js';
-import checkToken from '../middleware/checkToken.js';
+import checkToken from '../middleware/auth.js'; // Importa o middleware
 
 const router = express.Router();
 
